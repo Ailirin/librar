bd07e6a4cce243733b91f6e6186daf81e5ac766f
Ailirin <legenda93@yandex.ru>
2025-07-07 12:45:52 +0300

    Delete library_site/backend directory

--- library_site/backend/catalog/managenent.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth import get_user_model
-
-def create_superuser():
-    username, password = SUPERUSER_CREDENTIALS
-    User = get_user_model()
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, password=password)
